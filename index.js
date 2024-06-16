c8f540314de78cc10a32164235a8a297e4db78d4
Seyi Amusan <seyijoshua14@gmail.comgit config --global user.email seyijoshua14@gmail.com>
2024-06-16 10:25:44 +0100

    Pushing code to github

--- index.js
+++ index.js
@@ -5,18 +5,11 @@ const glide = new Glide('.glide', {
     
 }).mount();
 
+const glideTwo = new Glide('.glide-2', {
+    type: 'carousel',
+    perView: 1,
+    animationDuration: 1500, 
+    
+}).mount();
 
-let lastScrollLeft = 0;
-        window.addEventListener('scroll', () => {
-            const scrollLeft = window.scrollX || document.documentElement.scrollLeft;
 
-            if (scrollLeft > lastScrollLeft) {
-                // Scrolled right
-                glide.go('>>');
-            } else if (scrollLeft < lastScrollLeft) {
-                // Scrolled left
-                glide.go('<<');
-            }
-
-            lastScrollLeft = scrollLeft <= 0 ? 0 : scrollLeft; // For Mobile or negative scrolling
-        });
