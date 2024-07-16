29533a8cd5a94eb0fd17e9dc5d30773120d9c616
Seyi Amusan <seyijoshua14@gmail.comgit config --global user.email seyijoshua14@gmail.com>
2024-07-16 15:44:51 +0100

    Changed overall design layout for desktop viewers

---
+++ scripts/carousel.js
@@ -0,0 +1,13 @@
+const glide = new Glide('.glide', {
+    type: 'carousel',
+    perView: 1,
+    animationDuration: 1500, 
+    
+}).mount();
+
+const glideTwo = new Glide('.glide-2', {
+    type: 'carousel',
+    perView: 1,
+    animationDuration: 1500, 
+    
+}).mount();
--- scripts/scrollAnimation.js
+++ scripts/scrollAnimation.js
@@ -1,21 +1,3 @@
-//CAROUSEL
-const glide = new Glide('.glide', {
-    type: 'carousel',
-    perView: 1,
-    animationDuration: 1500, 
-    
-}).mount();
-
-const glideTwo = new Glide('.glide-2', {
-    type: 'carousel',
-    perView: 1,
-    animationDuration: 1500, 
-    
-}).mount();
-
-
-
-//SCROLL ANIMATION
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
