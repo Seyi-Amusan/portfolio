0e24efeaeb953ea94e4336fefef6e0e03141b5ce
Seyi Amusan <seyijoshua14@gmail.com>
2025-12-03 00:25:14 +0100

    Update Resume and Projects Section

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,19 +1,36 @@
-const glideLandingPages = new Glide('.glide-landing-pages', {
-  type: 'carousel',
-  startAt: 0,
-  perView: 1,
-  animationDuration: 700,
-  animationTimingFunc: 'ease-in-out'
+document.addEventListener("DOMContentLoaded", () => {
+  // Small delay to ensure all elements are rendered
+  setTimeout(() => {
+    initializeGlide();
+  }, 100);
 });
 
-glideLandingPages.mount();
-
-const glideReact = new Glide('.glide-react', {
-  type: 'carousel',
-  startAt: 0,
-  perView: 1,
-  animationDuration: 700,
-  animationTimingFunc: 'ease-in-out'
-});
-
-glideReact.mount();
+function initializeGlide() {
+  const glideReactEl = document.querySelector('.glide-react');
+  const glideLandingPagesEl = document.querySelector('.glide-landing-pages');
+  
+  console.log('Glide React element:', glideReactEl);
+  console.log('Glide Landing Pages element:', glideLandingPagesEl);
+  
+  if (glideReactEl) {
+    new Glide('.glide-react', {
+      type: 'carousel',
+      startAt: 0,
+      perView: 1,
+      animationDuration: 700,
+      animationTimingFunc: 'ease-in-out'
+    }).mount();
+    console.log('Glide React initialized');
+  }
+  
+  if (glideLandingPagesEl) {
+    new Glide('.glide-landing-pages', {
+      type: 'carousel',
+      startAt: 0,
+      perView: 1,
+      animationDuration: 700,
+      animationTimingFunc: 'ease-in-out'
+    }).mount();
+    console.log('Glide Landing Pages initialized');
+  }
+}
